59abf1ad85659e6653fb1efdd2b5252c7d6f2a04
rabosian <msann1356@gmail.com>
2024-10-19 15:12:46 -0400

    PrivateRoute added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
+import PrivateRoute from "./routes/PrivateRoute";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <Routes>
-      <Route path="/" element={<TodoPage />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute user={user}>
+            <TodoPage />
+          </PrivateRoute>
+        }
+      />
       <Route path="/register" element={<RegisterPage />} />
 
       <Route path="/login" element={<LoginPage />} />
